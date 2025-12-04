ec32987655c11a78e2e8ceba037966bb57599531
Valentin Pecatte <v.pecatte@gmail.com>
2025-12-04 18:40:59 +0100

    algo: insertion tri

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/forgeronvirtuel/algorithms-in-golang
+
+go 1.25.3
---
+++ internal/sort/insertion_sort_test.go
@@ -0,0 +1,30 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInsertionSort(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected []int
+	}{
+		{[]int{}, []int{}},
+		{[]int{1}, []int{1}},
+		{[]int{2, 1}, []int{1, 2}},
+		{[]int{5, 3, 8, 4, 2}, []int{2, 3, 4, 5, 8}},
+		{[]int{10, 9, 8, 7, 6}, []int{6, 7, 8, 9, 10}},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
+		{[]int{3, 3, 2, 1, 2}, []int{1, 2, 2, 3, 3}},
+	}
+
+	for _, tt := range tests {
+		arr := make([]int, len(tt.input))
+		copy(arr, tt.input)
+		InsertionSort(arr)
+		if !reflect.DeepEqual(arr, tt.expected) {
+			t.Errorf("InsertionSort(%v) = %v; want %v", tt.input, arr, tt.expected)
+		}
+	}
+}
